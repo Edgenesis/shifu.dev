d395da11c6f025155a882e3b182d21bd75d5ab6a
Yusufu1110 <29051099@qq.com>
2022-07-21 21:35:47 +0800

    modify title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Header from "../components/Homepage/download";
 
 export default function Hello() {
   return (
-    <Layout title="Hello" description="Hello React Page">
+    <Layout title="高效开发者工具，全场景物联网解决方案" description="让开发一个产业场景像开发一个APP一样简单！">
       <div className={styles.container}>
         <Header></Header>
         <HomepageCards title="我们的优势"></HomepageCards>
