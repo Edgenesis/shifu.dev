82a910073913101445db20e1c7fedc9255e4cc1f
Yusufu1110 <29051099@qq.com>
2022-08-15 12:31:17 +0800

    modify index color

--- src/components/Homepage/introduction/index.js
+++ src/components/Homepage/introduction/index.js
@@ -35,7 +35,7 @@ let developerlist = [
     descript: <span>大到使用私有驱动的工程机械<br />小到使用公有协议的温湿度计<br />你都可以让Shifu来帮你做对接异构设备的脏活累活</span>,
     isReverse: true,
     titleColor: "#2490FF",
-    shadowColor: "rgba(61, 114, 177, 0.1)"
+    shadowColor: "rgba(61, 114, 177, 0.2)"
   },
   {
     img: modularization,
@@ -43,7 +43,7 @@ let developerlist = [
     descript: <span>所有接入Shifu的设备都会被封装成一个拼图式模块<br />根据场景内的设备不同，按需加载即可</span>,
     isReverse: false,
     titleColor: "#C15CFF",
-    shadowColor: "rgba(126, 65, 155, 0.1)"
+    shadowColor: "rgba(126, 65, 155, 0.2)"
   },
   {
     img: application,
@@ -51,7 +51,7 @@ let developerlist = [
     descript: <span>接入设备后，Shifu会自动把设备抽象成微服务，让你的应用和硬件设备彻底解耦，把低效的物联网应用开发变得像面向对象编程一样高效。</span>,
     isReverse: true,
     titleColor: "#F33EE3",
-    shadowColor: "rgba(186, 90, 36, 0.1)"
+    shadowColor: "rgba(186, 90, 36, 0.2)"
   },
 ]
 
@@ -62,7 +62,7 @@ let engineeringList = [
     descript: <span>大到使用私有驱动的工程机械，小到使用公有协议的温湿度计，你都可以让Shifu来帮你做对接异构设备的脏活累活。</span>,
     isReverse: true,
     titleColor: "#FF64EF",
-    shadowColor: "rgba(200, 68, 39, 0.1)"
+    shadowColor: "rgba(200, 68, 39, 0.2)"
   },
   {
     img: security,
@@ -70,7 +70,7 @@ let engineeringList = [
     descript: <span>所有接入Shifu的设备都会被封装成一个乐高式模块，根据场景内的设备不同，按需加载即可。</span>,
     isReverse: false,
     titleColor: "#9A5CFF",
-    shadowColor: "rgba(128, 34, 186, 0.1)"
+    shadowColor: "rgba(128, 34, 186, 0.2)"
   },
   {
     img: community,
@@ -78,7 +78,7 @@ let engineeringList = [
     descript: <span>接入设备后，Shifu会自动把设备抽象成微服务，让你的应用和硬件设备彻底解耦，把低效的物联网应用开发变得像面向对象编程一样高效。</span>,
     isReverse: true,
     titleColor: "#2490FF",
-    shadowColor: "rgba(16, 83, 184, 0.1)"
+    shadowColor: "rgba(16, 83, 184, 0.2)"
   }
 ]
 
