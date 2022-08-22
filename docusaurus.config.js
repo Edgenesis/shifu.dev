ed996ee08384e27a0737f1203d06accdc74bd3d4
yxj <564197835@qq.com>
2022-08-22 17:07:08 +0800

    change black icon to blue icon which is better for Chrome in dark mode; remove useless docusaurus assets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -81,7 +81,7 @@ const config = {
         title: 'Shifu Framework',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/edgenesis.svg',
+          src: 'img/logo.svg',
         },
         items: [
           {
