81b10a9e633f35a1ca84f0b950c5cb0c28058601
Yusufu1110 <29051099@qq.com>
2022-08-24 00:03:29 +0800

    add footer and header

--- src/components/demo/button/index.js
+++ src/components/demo/button/index.js
@@ -2,10 +2,36 @@ import React from 'react';
 import styles from "./styles.module.scss";
 
 
-export function ButtonSquare() {
+export function ButtonSquare(props) {
+  let colorLevel = "";
+  switch (props.colorLevel) {
+    case "one":
+      colorLevel = "squareOne"
+      break
+    case "two":
+      colorLevel = "squareTwo"
+      break
+    case "three":
+      colorLevel = "squareThree"
+      break
+    default:
+      colorLevel = "squareOne"
+  }
   return (
-    <div className={styles.buttonContainer}>
-     
-    </div>
+    <a className={`${styles.buttonContainer} ${styles[colorLevel]} ${styles.square}`} href={props.href} target={props.target} onClick={props.onClick}>
+      <div className={styles.buttonLeft}>{props.left}</div>
+      <p className={styles.buttonContent}>{props.content}</p>
+      <div className={styles.buttonRight}>{props.right}</div>
+    </a >
+  )
+}
+
+export function ButtonTransparent(props) {
+  return (
+    <a className={`${styles.buttonContainer}  ${styles.transparent}`} href={props.href} target={props.target}>
+      <div className={styles.buttonLeft}>{props.left}</div>
+      <p className={styles.buttonContent}>{props.content}</p>
+      <div className={styles.buttonRight}>{props.right}</div>
+    </a >
   )
 }
--- src/components/demo/demoFooter/index.js
+++ src/components/demo/demoFooter/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles.module.scss";
+import { ButtonSquare, ButtonTransparent } from "../button";
 
+const GithubIcon = require('@site/static/img/icon/github-icon.svg').default
+const DocIcon = require('@site/static/img/icon/doc-icon.svg').default
 
-export default function DemoFooter() {
+function DemoFooter(props) {
+  let footerBtnContent;
+  switch (props.stepIndex) {
+    case 0:
+      footerBtnContent = "Docker部署已完成";
+      break;
+    case 1:
+      footerBtnContent = "Shifu安装已完成";
+      break;
+    case 2:
+      footerBtnContent = "进入Shifu Cloud";
+      break;
+    default:
+      footerBtnContent = "Docker部署已完成"
+  }
   return (
     <div className={styles.demoFooterContainer}>
-     
+      <div className={styles.demoFooterButton}>
+        <ButtonSquare colorLevel="two" content={footerBtnContent} onClick={props.onClick}></ButtonSquare>
+      </div>
+      <div className={styles.demoFooterContent}>
+        <div className={styles.demoFooterDoc}>
+          <ButtonTransparent content="技术文档" left={<DocIcon></DocIcon>} href="/"></ButtonTransparent>
+        </div>
+        <div className={styles.demoFooterGithub}>
+          <ButtonTransparent content="申请Github权限" left={<GithubIcon></GithubIcon>}></ButtonTransparent>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+export default DemoFooter
