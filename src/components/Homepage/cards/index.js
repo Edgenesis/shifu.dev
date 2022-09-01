30711d809ff18ea97dfd718cba253fa6a2dff4be
Yusufu1110 <29051099@qq.com>
2022-09-01 18:32:54 +0800

    modify the way of getting svg

--- src/components/Homepage/cards/index.js
+++ src/components/Homepage/cards/index.js
@@ -19,7 +19,7 @@ export function CardTwo(props) {
     <div className={styles.cardtwoContainer}>
       <div className={styles.cardTwoContent}>
         <div className={styles.cardTowIcon} style={{ backgroundColor: props.iconBg }}>
-          {props.img}
+          <props.img></props.img>
         </div>
         <h1 className={styles.cardTwoTitle}>{props.title}</h1>
         <p className={styles.cardTwoDescript}>{props.descript}</p>
