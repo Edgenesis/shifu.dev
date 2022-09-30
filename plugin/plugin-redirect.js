0d276e516900fd5ed9b3e1c27c5915cdc352c8fd
111fanyj <yfan@edgenesis.com>
2022-09-30 17:21:49 +0800

    feat 根据中英文差异化显示

--- plugin/plugin-redirect.js
+++ plugin/plugin-redirect.js
@@ -5,9 +5,19 @@ module.exports = function (context, options) {
       return {
         preBodyTags: [
           {
+            tagName: 'style',
+            attributes: {
+               id:'temp_showoff',
+            },
+            innerHTML: `
+              ._cn ,._en{
+                display:none
+              }
+      `,
+          },
+          {
             tagName: 'script',
-            innerHTML:
-            `
+            innerHTML: `
             let lang=navigator.languages;
             let href=window.location.href
             let host=window.location.port ? (document.domain + ':' + window.location.port ):document.domain 
@@ -19,7 +29,11 @@ module.exports = function (context, options) {
               elem.href += '?manually=true'
             }
           }, 100)
-            if(lang[0]== 'zh-CN' && href.indexOf('/zh-Hans') == -1 && href.indexOf('manually=true') == -1)
+            let manually= localStorage.getItem('manually')
+            console.log(manually)
+            console.log(document.html.className)
+            document.html.classList.add('CN');    
+            if(lang[0]== 'zh-CN' && href.indexOf('/zh-Hans') == -1 && !manually)
       {
         console.log('切换到汉语');
         document.location.href = href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
@@ -33,8 +47,58 @@ module.exports = function (context, options) {
       `,
           },
         ],
-  };
-},
-  };
-};
+        postBodyTags: [
+          {
+            tagName: 'script',
+            innerHTML: `
+            let time=0
+            console.log(document.body.className,document.className,document)
+            let predo=()=>{
+              time++
+              let href=window.location.href
+              if(href.indexOf('/zh-Hans')==-1)
+              document.body.classList.add('EN'); 
+              else
+              document.body.classList.add('CN'); 
+            }
+            let clearoff=()=>{
+              //删除之前双语的不显示css
+              //这样实现的效果是加载完成后再显示特定的语言 而不是 在开始就显示两种语言堆在一起
+              document.getElementById('temp_showoff').remove()
+            }
+            setTimeout(()=>{
+              predo()
+              clearoff()
+            }, 200)
+            //一次好像都不保险
+            setTimeout(()=>{
+              predo()
+              clearoff()
+            }, 400)
+            // 在0.07秒的之前的class 都会被清掉
+            document.body.classList.add('te');    
+            document.addEventListener("DOMContentLoaded", ()=>{
+              console.log('ready')
+              console.log(document.body.className)
+              document.body.classList.add('CN');  
+              console.log(document.body.className)
+            });
+      `,
+          },
+          {
+            tagName: 'style',
+            innerHTML: `
+              .EN ._cn{
+                display:none
+              }
 
+              .CN ._en{
+                display:none
+              }
+      `,
+          },
+        ],
+      }
+    },
+  }
+}
