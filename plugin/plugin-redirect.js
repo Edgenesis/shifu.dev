4b50c771cc47faa96671be8dff857b77ddbe8be8
slx <1564659409@qq.com>
2022-10-08 18:42:50 +0800

    中英文切换

--- plugin/plugin-redirect.js
+++ plugin/plugin-redirect.js
@@ -18,32 +18,72 @@ module.exports = function (context, options) {
           {
             tagName: 'script',
             innerHTML: `
+         
+         
+           
+     
+          window.addEventListener('load',function (){
             let lang=navigator.languages;
             let href=window.location.href
+            let protocol=window.location.protocol
             let host=window.location.port ? (document.domain + ':' + window.location.port ):document.domain 
-            setTimeout(()=> {
-            let elements = document.querySelectorAll('.dropdown__link')
-            console.log(elements);
-            for(let elem of elements) {
-              elem.onclick=function(){
-                localStorage.setItem('manuallySelectLanguage',true)
-              }
-            }
-          }, 100)
             let manually= localStorage.getItem('manuallySelectLanguage')
+            let baseHref=protocol+'//'+host
+               console.log(11111,protocol)
+               console.log(11111,protocol+host)
             console.log(manually)
-            if(lang[0]== 'zh-CN' && href.indexOf('/zh-Hans') == -1 && !manually)
-      {
-        console.log('切换到汉语');
-        document.location.href = href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
-      }
-      if(lang[0]!= 'zh-CN' && href.indexOf('/zh-Hans') != -1 &&  !manually)
-      {
-        console.log('changed into English');
-        console.log(href.split('/zh-Hans').join(''));
-        document.location.href = href.split('/zh-Hans').join('')
-      }
-      `,
+              if(manually){
+                  window.location=host+manually
+              }else{
+                 if(lang[0] == 'zh-CN'){
+                    console.log(444444,host+'/zh-Hans/')
+                   localStorage.setItem('manuallySelectLanguage','/zh-Hans/')
+                   window.location.href=baseHref+'/zh-Hans/'
+                  
+                 }else{
+                  console.log(5555)
+                   localStorage.setItem('manuallySelectLanguage','/')
+                   window.location.href= baseHref+'/'
+                 }
+              }
+              let elements = document.querySelectorAll('.dropdown__link')
+              for(let elem of elements) {
+                elem.onclick=function(){
+                 let nowHref=elem.getAttribute('href')
+                 console.log(nowHref)
+                 localStorage.setItem('manuallySelectLanguage',nowHref)
+                 window.location.href=baseHref+nowHref
+                 return false;
+                }
+              }
+            })
+            
+            
+            // setTimeout(()=> {
+            //   let elements = document.querySelectorAll('.dropdown__link')
+            //   console.log(22222,elements);
+            //   for(let elem of elements) {
+            //     elem.onclick=function(){
+            //       localStorage.setItem('manuallySelectLanguage',true)
+            //     }
+            //   }
+            // }, 100)
+            
+            // let manually= localStorage.getItem('manuallySelectLanguage')
+            // console.log(11111,lang)
+            // console.log(manually)
+            // if(lang[0]== 'zh-CN' && href.indexOf('/zh-Hans') == -1 && !manually)
+            //   {
+            //     console.log('切换到汉语');
+            //     document.location.href = href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
+            //   }
+            // if(lang[0]!= 'zh-CN' && href.indexOf('/zh-Hans') != -1 &&  !manually)
+            //   {
+            //     console.log('changed into English');
+            //     console.log(href.split('/zh-Hans').join(''));
+            //     document.location.href = href.split('/zh-Hans').join('')
+            //   }
+            `,
           },
         ],
         postBodyTags: [
@@ -56,9 +96,9 @@ module.exports = function (context, options) {
               time++
               let href=window.location.href
               if(href.indexOf('/zh-Hans')==-1)
-              document.body.classList.add('EN'); 
+              document.body.classList.add('EN');
               else
-              document.body.classList.add('CN'); 
+              document.body.classList.add('CN');
             }
             let clearoff=()=>{
               //删除之前双语的不显示css
@@ -75,11 +115,11 @@ module.exports = function (context, options) {
               clearoff()
             }, 400)
             // 在0.07秒的之前的class 都会被清掉
-            document.body.classList.add('te');    
+            document.body.classList.add('te');
             document.addEventListener("DOMContentLoaded", ()=>{
               console.log('ready')
               console.log(document.body.className)
-              document.body.classList.add('CN');  
+              document.body.classList.add('CN');
               console.log(document.body.className)
             });
       `,
