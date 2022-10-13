c8538c58522771ccac4f70a4bd459b1063894513
宋丽霞 <47417254+YAYAHEIYOU@users.noreply.github.com>
2022-10-13 17:42:11 +0800

    隐私协议中英文切换 (#141)
    * 隐私协议中英文切换
    中文情况下按钮跳转页面是英文网址
    
    * 删除注释

--- plugin/plugin-redirect.js
+++ plugin/plugin-redirect.js
@@ -10,9 +10,9 @@ module.exports = function (context, options) {
               id: 'temp_showoff',
             },
             innerHTML: `
-              .lang_zh, .lang_en {
-                display:none
-              }
+              // .lang_zh, .lang_en {
+              //   display:none
+              // }
       `,
           },
           {
@@ -25,29 +25,38 @@ module.exports = function (context, options) {
              * 后续如果检测到这个变量我们就不再做跳转了
              * 需要注意的问题是 窄屏下的多语言菜单只有在窄屏模式才会出现在DOM中
              * 而具体语言选择的按钮也只有只有点击多语言菜单才会出现在DOM中
-             * 解决方案是分别监听媒体查询和多语言按钮的点击 
-             * 然后保证具体的语言选择按钮出现时就带有点击事件 
+             * 解决方案是分别监听媒体查询和多语言按钮的点击
+             * 然后保证具体的语言选择按钮出现时就带有点击事件
              ***/
             innerHTML:
-              `
+                `
+                let href = window.location.href
+                let host = window.location.port
+                ? document.domain + ':' + window.location.port
+                : document.domain
             function autoredirect() {
               let manually = localStorage.getItem('manuallySelectLanguage')
               if (manually) {
+                if (href.indexOf('/zh-Hans')!==-1) {
+                    localStorage.setItem('manuallySelectLanguage','zh-Hans')
+                }else{
+                   localStorage.setItem('manuallySelectLanguage','en')
+                } 
                 return
               } else {
                 let lang = navigator.languages
-                let href = window.location.href
-                let host = window.location.port
-                  ? document.domain + ':' + window.location.port
-                  : document.domain
+              
+              
                 console.log('automatically change')
                 if (lang[0] == 'zh-CN' && href.indexOf('/zh-Hans') == -1) {
                   console.log('切换到汉语')
+                  localStorage.setItem('manuallySelectLanguage','zh-Hans')
                   document.location.href =
                     href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
                 }
                 if (lang[0] != 'zh-CN' && href.indexOf('/zh-Hans') != -1) {
                   console.log('changed into English')
+                   localStorage.setItem('manuallySelectLanguage','en')
                   document.location.href = href.split('/zh-Hans').join('')
                 }
               }
@@ -61,8 +70,18 @@ module.exports = function (context, options) {
               let elements = document.querySelectorAll('.langDropdown~ul .dropdown__link')
               console.log('wide', elements)
               for (let elem of elements) {
+                elem.setAttribute('href','javascript:void(0)')
                 elem.onclick = function () {
-                  localStorage.setItem('manuallySelectLanguage', true)
+                  let lang=elem.getAttribute('lang')
+                  localStorage.setItem('manuallySelectLanguage',lang)
+                  window.setTimeout(async () => {
+                    if (lang==='en') {
+                       document.location.href = href.split('/zh-Hans').join('')
+                    }else{
+                       document.location.href =
+                        href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
+                    }
+                 }, 100)
                 }
                 console.log(elem.getAttribute('href'))
               }
@@ -89,8 +108,19 @@ module.exports = function (context, options) {
                       )
                       console.log('narrow', elesEles)
                       for (let elem of elesEles) {
+                        elem.setAttribute('href','javascript:void(0)')
                         elem.onclick = function () {
-                          localStorage.setItem('manuallySelectLanguage', true)
+                          let lang=elem.getAttribute('lang')
+                          localStorage.setItem('manuallySelectLanguage',lang)
+                           window.setTimeout(async () => {
+                            if (lang==='en') {
+                               document.location.href = href.split('/zh-Hans').join('')
+                            }else{
+                               document.location.href =
+                                href.replace(\`\${host}\`, \`\${host}/zh-Hans\`) + ''
+                            }
+                           }, 100)
+                     
                         }
                         console.log(elem)
                       }
@@ -121,56 +151,9 @@ module.exports = function (context, options) {
             })
       `,
           },
-        ],
-        postBodyTags: [
-          {
-            tagName: 'script',
-            innerHTML: `
-            let href=window.location.href
-            let predo=()=>{
-              if(href.indexOf('/zh-Hans')==-1)
-              {
-                document.body.classList.add('EN'); 
-                console.log('add EN');
-              }
-              else
-               {
-                console.log('add CN');
-                document.body.classList.add('CN'); 
-               }
-            }
-            let clearoff=()=>{
-              //删除之前双语的不显示css
-              //这样实现的效果是加载完成后再显示特定的语言 而不是 在开始就显示两种语言堆在一起
-              document.getElementById('temp_showoff')?.remove()
-            }
-            // 在0.07秒的之前的class 都会被框架的js行为清掉
-            //为了保证这个效果的稳定实现 我们的策略是多次执行
-            document.addEventListener('DOMContentLoaded', () => {
-              console.log('ready')
-              let times = 0,
-                timer = setInterval(() => {
-                  if (times > 10) clearInterval(timer)
-                  times++
-                  predo()
-                  clearoff()
-                }, 200)
-            })
-      `,
-          },
-          {
-            tagName: 'style',
-            innerHTML: `
-              .EN .lang_zh{
-                display:none
-              }
-              .CN .lang_en{
-                display:none
-              }
-      `,
-          },
-        ],
+        ]
       }
     },
   }
 }
+
