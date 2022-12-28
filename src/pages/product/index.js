3bf8dfa444859526b9d62a1f9edd9632a8d240c3
Afei <1043164770@qq.com>
2022-12-28 17:08:36 +0800

    product -4

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -3,36 +3,9 @@ import React from 'react'
 import Layout from '@theme/Layout'
 import { Button, Row } from 'antd'
 import styles from './styles.module.scss'
-import PlanCard from './plancard/index'
+
 import Feature from './feature'
 import { Foot } from '../../components/footer/index'
-
-const list = [
-  {
-    title: 'Try open source Shifu framework',
-    instruction: [
-      'As ',
-      'a next-generation cloud-native IoT development framework',
-      ', Shifu provides users with full-scene device hosting and integrated software development capabilities. Through the use of the Shifu framework, developers can more easily connect, monitor and control any IoT device.'
-    ],
-    function: ['Fast Device Integration', 'Modularize Deployment Experience', 'Effective Application Development'],
-    button: 'Try it out',
-    jpg: 'github.png',
-    url: '/demo'
-  },
-  {
-    title: 'Try Shifu Cloud',
-    instruction: [
-      'As ',
-      'a device configuration platform based on the open source IoT development framework Shifu',
-      ', Shifu Cloud can automatically generate YAML files for the digital twin of the device, allowing developers to quickly obtain API interfaces for device capabilities and free up their hands for IoT scene development.'
-    ],
-    function: ['Multiple protocol support', 'Application Development Support', 'App Store Support'],
-    button: 'Free Access for Individuals',
-    jpg: 'cloud.png',
-    url: 'https://shifu.cloud/'
-  }
-]
 
 const list2 = [
   {
@@ -51,19 +24,6 @@ const list2 = [
     jpg: 'icon3.png'
   }
 ]
-
-const First = () => (
-  <>
-    <div className={styles.bck}></div>
-    <p className={styles.title} style={{ fontSize: 64 }}>
-      {translate({ message: 'Choose the deployment method that suits you and start your free trial now.' })}
-    </p>
-    <div className={styles.first}>
-      <PlanCard data={list[0]}></PlanCard>
-      <PlanCard data={list[1]}></PlanCard>
-    </div>
-  </>
-)
 
 export default function Disclaimer() {
   return (
