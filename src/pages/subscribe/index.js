014b7087b058de40a6e990f1996c7653e2527635
Afei <1043164770@qq.com>
2023-01-10 17:33:40 +0800

    wenhui-company

--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -16,14 +16,13 @@ export default class Hello extends Component {
             <div className={common.block80}></div>
             <div className={`${styles.thanksmain} ${common.content}`}>
               <div className={common.block80}></div>
-              <img src={require('@site/static/img/product/qrcode.png').default}></img>
-              <div className={common.block60}></div>
-              <p className={styles.thanksinstruction}>
-                {translate({
-                  message:
-                    'Edgenesis is dedicated to developing the next generation of open-source AIoT (artificial intelligence of things) operating system, with a craftsman spirit to build the infrastructure of the digital society. Follow our WeChat subscription account to stay updated on the latest information about Shifu.'
-                })}
-              </p>
+              <img src={require('@site/static/img/product/shifuLogo.png').default}></img>
+              <div className={common.block30}></div>
+              <p className={styles.thanksinstruction}>{translate({ message: 'Edgenesis is dedicated to developing the next generation of open-source AIoT (artificial intelligence of things) operating system, with a craftsman spirit to build the infrastructure of the digital society.' })}</p>
+              <p className={styles.thanksinstruction}>{translate({ message: 'Follow our WeChat subscription account to stay updated on the latest information about Shifu.' })}</p>
+              <div className={common.block30}></div>
+              <img className={styles.qrcode} src={require('@site/static/img/product/qrcode.png').default}></img>
+              <a href="/">{translate({ message: 'Redirecting to the homepage' })}</a>
               <div className={common.block80}></div>
             </div>
             <div className={common.block80}></div>
