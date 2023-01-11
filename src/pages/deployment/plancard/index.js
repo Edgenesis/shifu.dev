59b316ce5abd48d369bfd7d376bbe1bf28e9ae99
Afei <1043164770@qq.com>
2023-01-11 12:04:26 +0800

    wenhui-20230111

--- src/pages/deployment/plancard/index.js
+++ src/pages/deployment/plancard/index.js
@@ -1,7 +1,7 @@
-import {Button, Space} from 'antd'
-import Translate,{translate} from '@docusaurus/Translate'
+import { Button, Space } from 'antd'
+import Translate, { translate } from '@docusaurus/Translate'
 import styles from './styles.module.scss'
-import React, {Component, forwardRef} from 'react'
+import React, { Component, forwardRef } from 'react'
 import gouxuan from '@site/static/img/product/gouxuan.png'
 import common from '@site/src/css/common.module.scss'
 
@@ -16,13 +16,13 @@ const list = [
     ],
     function: ['Fast Device Integration', 'Modularize Deployment Experience', 'Effective Application Development'],
     button: 'Try it out',
-    jpg: 'github.png',
+    jpg: 'GitHub.svg',
     url: 'demo',
     css: 'blue'
   },
   {
-    title: '',
-    title2: 'Try Shifu Cloud',
+    title: 'Try Production-Grade',
+    title2: 'Shifu Cloud',
     instruction: [
       'As ',
       'a device configuration platform based on the open source IoT development framework Shifu',
@@ -30,54 +30,53 @@ const list = [
     ],
     function: ['Multiple protocol support', 'Application Development Support', 'App Store Support'],
     button: 'Free Access for Individuals',
-    jpg: 'cloud.png',
+    jpg: 'cloud.svg',
     url: 'bookDemo',
     css: 'blue'
   }
 ]
 const PlanCard = forwardRef(props => {
   return (
-      <div className={[styles.PlanCard, styles[props.data.css]].join(' ')}>
-        <div className={styles.planCardCon}>
-          <div className={styles.title}>
-            <div className={styles.imgBox}>
-              <img src={require('@site/static/img/product/' + props.data.jpg).default}></img>
-            </div>
-
-            <div>
-              <h2>{translate({message: props.data.title})}</h2>
-              <h1>{translate({message: props.data.title2})}</h1>
-            </div>
+    <div className={[styles.PlanCard, styles[props.data.css]].join(' ')}>
+      <div className={styles.planCardCon}>
+        <div className={styles.title}>
+          <div className={styles.imgBox}>
+            <img src={require('@site/static/img/product/' + props.data.jpg).default}></img>
           </div>
-          <div className={styles.instruction}>
-            <div className={common.block50}></div>
-            <p className={styles.instructionmain}>
-              {translate({message: props.data.instruction[0]})}
-              <a>{translate({message: props.data.instruction[1]})}</a>
-              {translate({message: props.data.instruction[2]})}
-            </p>
-            <div className={common.block50}></div>
-            <div className={styles.functionlist}>
-              <ul>
-                {props.data.function.map((item, index) => {
-                  return (
-                      <li key={item}>
-                        <img src={gouxuan} alt=""></img>
-                        {translate({message: item})}
-                      </li>
-                  )
-                })}
-              </ul>
-            </div>
-            <div className={common.block50}></div>
-            <Button type="primary" block size="large" href={`${props.url}${props.data.url}`} className={styles.button}>
-              <Translate>{props.data.button}</Translate>
-            </Button>
-            <div className={common.block60}></div>
+
+          <div>
+            <h2>{translate({ message: props.data.title })}</h2>
+            <h1>{translate({ message: props.data.title2 })}</h1>
           </div>
         </div>
-
+        <div className={styles.instruction}>
+          <div className={common.block50}></div>
+          <p className={`${styles.instructionmain} ${styles[props.class]}`}>
+            {translate({ message: props.data.instruction[0] })}
+            <a>{translate({ message: props.data.instruction[1] })}</a>
+            {translate({ message: props.data.instruction[2] })}
+          </p>
+          <div className={common.block50}></div>
+          <div className={styles.functionlist}>
+            <ul>
+              {props.data.function.map((item, index) => {
+                return (
+                  <li key={item}>
+                    <img src={gouxuan} alt=""></img>
+                    {translate({ message: item })}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className={common.block50}></div>
+          <Button type="primary" block size="large" href={`${props.url}${props.data.url}`} className={styles.button}>
+            <Translate>{props.data.button}</Translate>
+          </Button>
+          <div className={common.block60}></div>
+        </div>
       </div>
+    </div>
   )
 })
 
@@ -86,32 +85,33 @@ export default class PlanCardList extends Component {
     super(props)
     this.state = {
       url: '/',
+      class: ''
     }
   }
 
   componentDidMount() {
     if (window.location.href.includes('zh-Hans')) {
       this.setState({
-        url: '/zh-Hans/'
+        url: '/zh-Hans/',
+        class: 'hans'
       })
     } else {
       this.setState({
-        url: '/'
+        url: '/',
+        class: ''
       })
     }
-
   }
 
   render() {
     return (
-        <>
-          <div className={styles.list}>
-            <PlanCard data={list[0]} url={this.state.url}></PlanCard>
-            <PlanCard data={list[1]} url={this.state.url}></PlanCard>
-          </div>
-          <div className={common.block80}></div>
-        </>
+      <>
+        <div className={styles.list}>
+          <PlanCard data={list[0]} url={this.state.url} class={this.state.class}></PlanCard>
+          <PlanCard data={list[1]} url={this.state.url} class={this.state.class}></PlanCard>
+        </div>
+        <div className={common.block80}></div>
+      </>
     )
   }
-
 }
