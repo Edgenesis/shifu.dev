d7c70ba75b7cbe5c072c5d3dc1393f912847c43e
宋丽霞 <47417254+YAYAHEIYOU@users.noreply.github.com>
2023-03-21 14:46:38 +0800

    Hide filing number (#245)

--- plugin/plugin-redirect.js
+++ plugin/plugin-redirect.js
@@ -160,6 +160,111 @@ module.exports = function (context, options) {
       `,
           },
         ],
+        postBodyTags: [
+          {
+            tagName: 'script',
+            innerHTML: `
+            class Dep {
+              // 订阅池
+              constructor(name) {
+                  this.id = new Date(); //这里简单的运用时间戳做订阅池的ID
+                  this.subs = []; //该事件下被订阅对象的集合
+              }
+                  defined() {
+                      // 添加订阅者
+                      Dep.watch.add(this);
+                  }
+                  notify() {
+                      //通知订阅者有变化
+                      this.subs.forEach((e, i) => {
+                          if (typeof e.update === 'function') {
+                              try {
+                                e.update.apply(e); //触发订阅者更新函数
+                              } catch (err) {
+                                  console.warr(err);
+                              }
+                          }
+                      });
+                  }
+              }
+              Dep.watch = null;
+              
+              class Watch {
+                  constructor(name, fn) {
+                      this.name = name; //订阅消息的名称
+                      this.id = new Date(); //这里简单的运用时间戳做订阅者的ID
+                      this.callBack = fn; //订阅消息发送改变时->订阅者执行的回调函数
+                  }
+                  add(dep) {
+                      //将订阅者放入dep订阅池
+                      dep.subs.push(this);
+                  }
+                  update() {
+                      //将订阅者更新方法
+                      var cb = this.callBack; //赋值为了不改变函数内调用的this
+                      cb(this.name);
+                  }
+            }
+          
+            var addHistoryMethod = (function () {
+              var historyDep = new Dep();
+              return function (name) {
+                  if (name === 'historychange') {
+                      return function (name, fn) {
+                          // 订阅一下
+                          var event = new Watch(name, fn);
+                          Dep.watch = event;
+                          historyDep.defined();
+                          Dep.watch = null; //置空供下一个订阅者使用
+                      }
+                  }else if (name === 'pushState' || name === 'replaceState') {
+                      var method = history[name];
+                      return function () {
+                          /**
+                           * 1，arguments 是每一个函数都自带的属性
+                           * 2，arguments 会自动的将所有的实参存储其中
+                           * **/
+                          method.apply(history, arguments);
+                          historyDep.notify();
+                      }
+                  }
+              }
+          })();
+
+          window.addHistoryListener = addHistoryMethod('historychange');
+          history.pushState = addHistoryMethod('pushState');
+          history.replaceState = addHistoryMethod('replaceState');
+
+          function addHide(){
+            let timer = setInterval(() => {
+              if(!document.getElementById('foot').classList.contains('hide')){
+                document.getElementById('foot').classList.add('hide')
+              }else{
+                clearInterval(timer)
+              }
+            }, 200)
+           
+          }
+
+          function addClass(){
+            let href = window.location.href
+            if(href.includes('shifu.dev')){
+              addHide()
+            }
+          }
+
+          window.addHistoryListener('history', function () {
+            addClass()
+          });
+          document.addEventListener('DOMContentLoaded', () => {
+            addClass()
+          })
+
+            `
+            
+          }
+
+        ],
       }
     },
   }
