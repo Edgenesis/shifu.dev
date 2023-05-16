4591d208638a5b595ac0427662567b73d04d5311
lijundi <34619020+lijundi@users.noreply.github.com>
2023-05-16 09:20:43 +0800

    bump version to v0.18.0 (#256)
    Co-authored-by: lijundi <lijundi@edgenesis.com>

--- src/components/home/News/index.js
+++ src/components/home/News/index.js
@@ -26,8 +26,8 @@ let Lists = [
     img: 'releases.svg',
     // img: require('@site/static/img/home/releases.png').default,
     title: translate({ message: 'Releases' }),
-    message: translate({ message: 'v0.17.0' }),
-    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.17.0',
+    message: translate({ message: 'v0.18.0' }),
+    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.18.0',
     link: 'https://github.com/Edgenesis/shifu/releases'
   }
 ]
