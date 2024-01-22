98acbb25387a1cc5dcb46edaadfdb21b5c9aead8
RyougiShiki <53469345+ryougi-shiky@users.noreply.github.com>
2024-01-22 19:25:43 +0800

    update tag

--- src/components/home/News/index.js
+++ src/components/home/News/index.js
@@ -26,8 +26,8 @@ let Lists = [
     img: 'releases.svg',
     // img: require('@site/static/img/home/releases.png').default,
     title: translate({ message: 'Releases' }),
-    message: translate({ message: 'v0.35.0' }),
-    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.35.0',
+    message: translate({ message: 'v0.36.0' }),
+    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.36.0',
     link: 'https://github.com/Edgenesis/shifu/releases'
   }
 ]
