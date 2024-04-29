3e6788f607edd8342d9ec834e85e1733f7975dcb
Tom Qin <xqin@edgenesis.com>
2024-04-29 17:31:08 +0800

    update Shifu to v0.43.0

--- src/components/home/News/index.js
+++ src/components/home/News/index.js
@@ -26,8 +26,8 @@ let Lists = [
     img: 'releases.svg',
     // img: require('@site/static/img/home/releases.png').default,
     title: translate({ message: 'Releases' }),
-    message: translate({ message: 'v0.42.0' }),
-    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.42.0',
+    message: translate({ message: 'v0.43.0' }),
+    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.43.0',
     link: 'https://github.com/Edgenesis/shifu/releases'
   }
 ]
