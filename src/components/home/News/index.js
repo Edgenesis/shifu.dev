b30076b94cd9637031fafbd938a7281927638603
Tom Qin <xqin@edgenesis.com>
2024-09-30 20:02:20 +0900

    bump Shifu to v0.54.0 (#321)

--- src/components/home/News/index.js
+++ src/components/home/News/index.js
@@ -26,8 +26,8 @@ let Lists = [
     img: 'releases.svg',
     // img: require('@site/static/img/home/releases.png').default,
     title: translate({ message: 'Releases' }),
-    message: translate({ message: 'v0.53.0' }),
-    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.53.0',
+    message: translate({ message: 'v0.54.0' }),
+    messageLink: 'https://github.com/Edgenesis/shifu/releases/tag/v0.54.0',
     link: 'https://github.com/Edgenesis/shifu/releases'
   }
 ]
